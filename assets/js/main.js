95f2e7e673d1981a2987a4e30156f654f8c983ea
hritvik1 <hritvikmaini00@gmail.com>
2021-04-11 17:24:19 +0530

    View & JS Updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,7 @@ const covidAPI_1 = 'https://api.covid19api.com/country/',
 
 async function submitData() {
   event.preventDefault()
+  document.getElementById("display_cases").style.display = "none"
 
   const countryName = document.getElementById('countryName').value,
         startDate   = document.getElementById('startDate').value,
@@ -17,20 +18,34 @@ async function submitData() {
 
   const finalApiUrl = covidAPI_1 + countryName + covidAPI_2 + startDate + covidAPI_3 + covidAPI_4 + endDate + covidAPI_3
 
-  fetch(finalApiUrl)
-      .then((response) => response.json())
-      .then((response) => {
-          const length = response.length,
-                index  = length - 1
+  if(countryName === '' || startDate === '' || endDate === '') {
+    alert("Please Fill Complete Form!!!!")
+  }
+  else {
+      document.getElementById("loading").style.display = "block"
 
-          const conf = response[index].Confirmed,
-                act = response[index].Active,
-                dth = response[index].Deaths
+      await fetch(finalApiUrl)
+          .then(response => response.json())
+          .then(response => {
+              const length = response.length,
+                  index  = length - 1
 
-          confirmed.innerHTML = conf
-          active.innerHTML    = act
-          deaths.innerHTML    = dth
+              const conf = response[index].Confirmed,
+                  act = response[index].Active,
+                  dth = response[index].Deaths
 
-          document.getElementById("display_cases").style.display = "block"
-      })
+              confirmed.innerHTML = conf
+              active.innerHTML    = act
+              deaths.innerHTML    = dth
+
+              document.getElementById("main_form").reset()
+              document.getElementById("loading").style.display = "none"
+              document.getElementById("display_cases").style.display = "block"
+          })
+          .catch(function(error) {
+            console.log(error)
+            document.getElementById("loading").style.display = "none"
+            alert('Error fetching data or Data not available.. TRY AGAIN......')
+          })
+  }
 }
